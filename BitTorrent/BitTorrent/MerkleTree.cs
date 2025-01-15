3d3bc432ee475686741596e0c07ca125201449b0
Artur <guart05@mail.ru>
2025-01-15 14:48:39 +0300

    added simple realization of Merkle Tree

---
+++ BitTorrent/BitTorrent/MerkleTree.cs
@@ -0,0 +1,120 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BitTorrent;
+
+class MerkleTree
+{
+    public class Node
+    {
+        public string Hash { get; set; }
+        public Node Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(string hash)
+        {
+            Hash = hash;
+        }
+    }
+
+    public Node Root { get; private set; }
+    private readonly List<string> _leafHashes;
+
+    public MerkleTree(List<string> dataBlocks)
+    {
+        _leafHashes = dataBlocks.Select(ComputeHash).ToList();
+        BuildTree();
+    }
+
+    private void BuildTree()
+    {
+        var nodes = _leafHashes
+            .Select(hash => new Node(hash))
+            .ToList();
+
+        while (nodes.Count > 1)
+        {
+            var parentNodes = new List<Node>();
+
+            for (int i = 0; i < nodes.Count; i += 2)
+            {
+                var left = nodes[i];
+                var right = i + 1 < nodes.Count
+                    ? nodes[i + 1]
+                    : null;
+
+                var parentHash = right != null
+                    ? ComputeHash(left.Hash + right.Hash)
+                    : left.Hash;
+
+                parentNodes.Add(new Node(parentHash) { Left = left, Right = right });
+            }
+
+            nodes = parentNodes;
+        }
+
+        Root = nodes.First();
+    }
+
+    public List<string> GetAuditPath(int index)
+    {
+        var path = new List<string>();
+        var nodes = _leafHashes
+            .Select(hash => new Node(hash))
+            .ToList();
+
+        while (nodes.Count > 1)
+        {
+            var parentNodes = new List<Node>();
+            for (int i = 0; i < nodes.Count; i += 2)
+            {
+                var left = nodes[i];
+                var right = i + 1 < nodes.Count ? nodes[i + 1] : null;
+                var parentHash = right != null ? ComputeHash(left.Hash + right.Hash) : left.Hash;
+
+                parentNodes.Add(new Node(parentHash) { Left = left, Right = right });
+
+                if (i != index && i + 1 != index) continue;
+                
+                var sibling = i == index
+                    ? right
+                    : left;
+                
+                if (sibling != null)
+                {
+                    path.Add(sibling.Hash);
+                }
+
+                index /= 2;
+            }
+
+            nodes = parentNodes;
+        }
+
+        return path;
+    }
+
+
+    public bool VerifyBlock(string block, int index, List<string> auditPath)
+    {
+        string currentHash = ComputeHash(block);
+
+        foreach (var siblingHash in auditPath)
+        {
+            currentHash = index % 2 == 0
+                ? ComputeHash(currentHash + siblingHash)
+                : ComputeHash(siblingHash + currentHash);
+
+            index /= 2;
+        }
+
+        return currentHash == Root.Hash;
+    }
+
+    public string ComputeHash(string data)
+    {
+        using var sha256 = SHA256.Create();
+        var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(data));
+        return Convert.ToBase64String(hash);
+    }
+}
---
+++ BitTorrent/BitTorrent/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BitTorrent;
+
+static class Program
+{
+    static void Main()
+    {
+        var dataBlocks = new List<string> {
+            "Block1",
+            "Block2", 
+            "Block3",
+            "Block4" };
+        
+        var merkleTree = new MerkleTree(dataBlocks);
+        Console.WriteLine($"Root Hash: {merkleTree.Root.Hash}");
+
+        var firstHash = merkleTree.ComputeHash(dataBlocks[0]);
+        var secondHash = merkleTree.ComputeHash(dataBlocks[1]);
+        var thirdHash = merkleTree.ComputeHash(dataBlocks[2]);
+        var fourthHash = merkleTree.ComputeHash(dataBlocks[3]);
+
+        Console.WriteLine(firstHash, secondHash, thirdHash, fourthHash);
+        
+        int blockIndex = 1; 
+        string blockData = dataBlocks[blockIndex];
+        
+        // Это хеши всех парных элементов отн-о данного, чтоб вычислить потом вершину
+        // Конкретнее: нужно, чтобы вычислять родителей каждого элемента (тк везде нужна пара)
+        var auditPath = merkleTree.GetAuditPath(blockIndex);
+        
+        // А здесь мы берем наш элемент и от него начинаем идти вверх и сравниваем вершину, если равны, то все ок
+        bool isValid = merkleTree
+            .VerifyBlock(blockData, blockIndex, auditPath);
+        
+        Console.WriteLine($"Block {blockIndex} verification:" +
+                          $" {(isValid ? "Valid" : "Invalid")}");
+    }
+}
