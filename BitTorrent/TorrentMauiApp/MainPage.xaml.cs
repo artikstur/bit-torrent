1d74a0e65829e7c875d12a71421918be2d8e7d4d
cl9wnn <egor_udir@mail.ru>
2025-01-29 15:33:28 +0300

    Добавил на главную страницу нужные кнопки и ивенты

--- BitTorrent/TorrentMauiApp/MainPage.xaml.cs
+++ BitTorrent/TorrentMauiApp/MainPage.xaml.cs
@@ -2,22 +2,40 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OnSelectFileClicked(object sender, EventArgs e)
     {
-        count++;
+        var result = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Выберите JSON-файл",
+            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.WinUI, new[] { ".json" } }
+            })
+        });
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+        if (result != null)
+        {
+            FilePathEntry.Text = result.FullPath; 
+        }
+    }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    private async void OnStartDownloadClicked(object sender, EventArgs e)
+    {
+        // Логика для начала загрузки
+    }
+
+    private async void OnStopDownloadClicked(object sender, EventArgs e)
+    {
+        // Логика для остановки загрузки
+    }
+
+    private async void OnStopDistributionClicked(object sender, EventArgs e)
+    {
+        // Логика для остановки раздачи
     }
 }
