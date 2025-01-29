a4e1f1dc12437badacc695adb2b263f1384c988b
cl9wnn <egor_udir@mail.ru>
2025-01-29 12:23:44 +0300

    Добавил шаблон MAUI приложения

---
+++ BitTorrent/TorrentMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TorrentMauiApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ BitTorrent/TorrentMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TorrentMauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
